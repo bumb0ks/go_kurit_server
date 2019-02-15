c703459a8ad3e4bcb98d085a8d1595db90ea53dd
bumb0ks <bum.boks@live.ru>
2019-02-15 17:57:36 +0300

    Удалил артефакты

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bumb0ks/Projects/go_kurit_server
-# Build directory: /home/bumb0ks/Projects/go_kurit_server/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
